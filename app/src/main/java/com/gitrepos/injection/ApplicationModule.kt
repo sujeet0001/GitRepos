716ee99dab16aad7bc74f9b2170bdaec584e44f6
sujeet0001 <sujeet0001@yahoo.com>
2022-07-22 12:06:59 +0530

    Binding instance of RoomDB

--- app/src/main/java/com/gitrepos/injection/ApplicationModule.kt
+++ app/src/main/java/com/gitrepos/injection/ApplicationModule.kt
@@ -1,5 +1,9 @@
 package com.gitrepos.injection
 
+import android.content.Context
+import androidx.room.Room
+import com.gitrepos.room.RepoDB
+import com.gitrepos.utils.ApplicationContext
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
@@ -16,6 +20,17 @@ class ApplicationModule {
         return Retrofit.Builder().baseUrl("https://api.github.com/users/")
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRoomDB(@ApplicationContext context: Context): RepoDB {
+        return Room.databaseBuilder(
+            context,
+            RepoDB::class.java, "REPO_DATABASE" // database name need to be stored elsewhere
+        )
+            .fallbackToDestructiveMigration() // get correct db version if schema changed
             .build()
     }
 }
